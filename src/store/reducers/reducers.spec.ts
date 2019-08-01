495e8a87a935487ff571c7c6294ac253f14a0a25
arnau <arnausd23@hotmail.com>
2019-08-01 14:22:25 +0200

    Added Jest config with Typescript and a reducer test

---
+++ src/store/reducers/reducers.spec.ts
@@ -0,0 +1,25 @@
+import {Player} from "../../models/Player";
+import {IInitialState} from "../reducers/reducer";
+
+import * as fromActions from '../actions/actions';
+import * as fromReducer from '../reducers/reducer';
+
+describe('Reducer tests' , () => {
+    it('[ADD_PLAYER] should add a player to the players array', () => {
+        const player: Player = Player.fromJSON({
+            id: '1',
+            userName: 'arnau',
+            email: 'arnausd23@hotmail.com',
+            password: '123',
+        });
+
+        const initialState: IInitialState = {
+            players: [],
+        };
+
+        const action = fromActions.addPlayer(player);
+        const state = fromReducer.playerReducer(initialState, action);
+
+        expect(state.players).toEqual([player]);
+    });
+});
