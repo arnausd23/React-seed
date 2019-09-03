3beaf955c0a08dfba7536b29a087d9f48a8838d0
arnausilvestre <arnau.silvestre@apiumhub.com>
2019-09-03 11:34:44 +0200

    Added title to homepage

--- src/app/_shared/router/router.tsx
+++ src/app/_shared/router/router.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import AuthRouter from "../../Auth/router/router";
 import PlayerRouter from "../../Player/router/router";
 
+const Home = () => <h1>Home</h1>;
+
 function AppRouter() {
     return (
         <Router>
+            <Route exact path="/" component={Home}/>
             <AuthRouter/>
             <PlayerRouter />
         </Router>
